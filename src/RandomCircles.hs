87355f61703dabc0e5322c50bef08f8717938647
bewakes <spirit_bibek@yahoo.com>
2019-03-24 20:19:40 +0545

    Refactor

--- src/RandomCircles.hs
+++ src/RandomCircles.hs
@@ -5,12 +5,10 @@ module RandomCircles (
 import System.Random
 import Control.Monad (replicateM)
 
-import Utils.Geometry
+import qualified Utils.Geometry as UG
 
 import qualified Graphics.Gloss as G
 
-
-type Circle = (Float, Float, Float)
 
 width = 500
 height = 700
@@ -34,27 +32,14 @@ getBoundedValue min max value = min + value * (max - min)
 nRandomFloats :: Int -> IO [Float]
 nRandomFloats count = replicateM count (randomIO :: IO Float)
 
-doCirclesIntersect :: Circle -> Circle -> Float -> Bool
-doCirclesIntersect (x1, y1, r1) (x2, y2, r2) offset = distance (x1, y1) (x2, y2) < r1 + r2 + offset
-
-doesCircleIntercectWithAny :: Circle -> [Circle] -> Float -> Bool
-doesCircleIntercectWithAny circle circles offset = foldl
-    (\a x -> doCirclesIntersect circle x offset || a) False circles
-
-getMaxNonIntersectingCircle :: Circle -> [Circle] -> Float -> Circle
+getMaxNonIntersectingCircle :: UG.Circle -> [UG.Circle] -> Float -> UG.Circle
 getMaxNonIntersectingCircle circle circles offset = getMaxCircleLimit 0 circle circles offset
     where
         getMaxCircleLimit cnt (x, y, r) cs o
-            | doesCircleIntercectWithAny (x, y, r+5) circles offset || cnt > 20 || r >= maxRad = (x, y, r)
+            | UG.doesCircleIntercectWithAny (x, y, r+5) circles offset || cnt > 20 || r >= maxRad = (x, y, r)
             | otherwise = getMaxCircleLimit (cnt+1) (x, y, r+5) circles offset
 
-run :: IO ()
-run = do
-    circles <- getCircles count
-    G.display (G.InWindow "Random Circles" widthHeight (20,20)) G.black circles
-
-
-generateNonIntersectingCircles :: Int -> IO [Circle]
+generateNonIntersectingCircles :: Int -> IO [UG.Circle]
 generateNonIntersectingCircles count = do
     (_, circles) <- f (pure (count, []))
     return circles
@@ -64,19 +49,24 @@ generateNonIntersectingCircles count = do
             circle <- generateRandomCircle
             if cnt == 0 then return (cnt, circles)
             else
-                if doesCircleIntercectWithAny circle circles circlesOffset then f state
+                if UG.doesCircleIntercectWithAny circle circles circlesOffset then f state
                 else let maxCircle = getMaxNonIntersectingCircle circle circles circlesOffset
                     in f (pure (cnt - 1, maxCircle : circles))
 
-generateRandomCircle :: IO Circle
+generateRandomCircle :: IO UG.Circle
 generateRandomCircle = do
     x <- getBoundedValue minX maxX <$> (randomIO :: IO Float)
     y <- getBoundedValue minY maxY <$> (randomIO :: IO Float)
     r <- getBoundedValue minRad maxRad <$> (randomIO :: IO Float)
     return (x, y, r)
 
-getCircle :: Circle -> G.Picture
+getCircle :: UG.Circle -> G.Picture
 getCircle (x, y, r) = G.Color G.white $ G.Translate x y $ G.ThickCircle 0 (2*r)
 
 getCircles :: Int -> IO G.Picture
 getCircles count = G.Pictures . map getCircle <$> generateNonIntersectingCircles count
+
+run :: IO ()
+run = do
+    circles <- getCircles count
+    G.display (G.InWindow "Random Circles" widthHeight (20,20)) G.black circles
--- src/Utils/Geometry.hs
+++ src/Utils/Geometry.hs
@@ -1,5 +1,14 @@
 module Utils.Geometry where
 
+type Circle = (Float, Float, Float)
 
 distance :: (Float, Float) -> (Float, Float) -> Float
 distance (a, b) (c, d) = sqrt $ (a - c) * (a - c) + (b - d) * (b - d)
+
+doCirclesIntersect :: Circle -> Circle -> Float -> Bool
+doCirclesIntersect (x1, y1, r1) (x2, y2, r2) offset = distance (x1, y1) (x2, y2) < r1 + r2 + offset
+
+doesCircleIntercectWithAny :: Circle -> [Circle] -> Float -> Bool
+doesCircleIntercectWithAny circle circles offset = foldl
+    (\a x -> doCirclesIntersect circle x offset || a) False circles
+
