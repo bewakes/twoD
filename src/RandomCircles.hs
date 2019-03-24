a782f64279c2fe418e9d5dc46f3c49112e4baaef
bewakes <spirit_bibek@yahoo.com>
2019-03-24 12:05:34 +0545

    Fix random circles

--- src/RandomCircles.hs
+++ src/RandomCircles.hs
@@ -12,9 +12,9 @@ import qualified Graphics.Gloss as G
 
 type Circle = (Float, Float, Float)
 
-width = 700
-height = 1000
-maxRad = 100
+width = 500
+height = 700
+maxRad = 80
 minRad = 10
 
 maxX = width / 2
@@ -24,6 +24,7 @@ maxY = height / 2
 minY = -maxY
 
 count = 100
+circlesOffset = 0
 
 widthHeight = (round width, round height)
 
@@ -34,11 +35,18 @@ nRandomFloats :: Int -> IO [Float]
 nRandomFloats count = replicateM count (randomIO :: IO Float)
 
 doCirclesIntersect :: Circle -> Circle -> Float -> Bool
-doCirclesIntersect (x1, y1, r1) (x2, y2, r2) offset = distance (x1, y1) (x2, y2) <= r1 + r2 + offset
+doCirclesIntersect (x1, y1, r1) (x2, y2, r2) offset = distance (x1, y1) (x2, y2) < r1 + r2 + offset
 
 doesCircleIntercectWithAny :: Circle -> [Circle] -> Float -> Bool
 doesCircleIntercectWithAny circle circles offset = foldl
     (\a x -> doCirclesIntersect circle x offset || a) False circles
+
+getMaxNonIntersectingCircle :: Circle -> [Circle] -> Float -> Circle
+getMaxNonIntersectingCircle circle circles offset = getMaxCircleLimit 0 circle circles offset
+    where
+        getMaxCircleLimit cnt (x, y, r) cs o
+            | doesCircleIntercectWithAny (x, y, r+5) circles offset || cnt > 20 || r >= maxRad = (x, y, r)
+            | otherwise = getMaxCircleLimit (cnt+1) (x, y, r+5) circles offset
 
 run :: IO ()
 run = do
@@ -56,8 +64,9 @@ generateNonIntersectingCircles count = do
             circle <- generateRandomCircle
             if cnt == 0 then return (cnt, circles)
             else
-                if doesCircleIntercectWithAny circle circles 0 then f state
-                else f (pure (cnt - 1, circle: circles))
+                if doesCircleIntercectWithAny circle circles circlesOffset then f state
+                else let maxCircle = getMaxNonIntersectingCircle circle circles circlesOffset
+                    in f (pure (cnt - 1, maxCircle : circles))
 
 generateRandomCircle :: IO Circle
 generateRandomCircle = do
@@ -67,7 +76,7 @@ generateRandomCircle = do
     return (x, y, r)
 
 getCircle :: Circle -> G.Picture
-getCircle (x, y, r) = G.Color G.white $ G.Translate x y $ G.ThickCircle 0 r
+getCircle (x, y, r) = G.Color G.white $ G.Translate x y $ G.ThickCircle 0 (2*r)
 
 getCircles :: Int -> IO G.Picture
 getCircles count = G.Pictures . map getCircle <$> generateNonIntersectingCircles count
