81e628abba7987facd8f36d8e6386fe189ecdb73
bewakes <spirit_bibek@yahoo.com>
2019-04-03 08:19:06 +0545

    Sierpinski triangle

--- src/Fractals.hs
+++ src/Fractals.hs
@@ -25,16 +25,20 @@ getChildren (UG.Triangle a b c) = [
     , getTriangleForSide scaleFactor (c, a)
     ]
 
-
 getTriangleForSide :: Float -> UG.Line -> UG.Triangle
 getTriangleForSide scale (p1, p2) = UG.Triangle a b c
     where a = UG.midPoint p1 p2
           (_, b) = (UG.fractionLine scale . UG.rotateLineAbout a piBy3) (a, p1)
           (_, c) = (UG.fractionLine scale . UG.rotateLineAbout a (2*piBy3)) (a, p1)
 
+iterateTriangles :: Int -> UG.Triangle -> [UG.Triangle]
+iterateTriangles 0 triangle = []
+iterateTriangles count triangle = triangle: concatMap nextIterate children
+    where nextIterate = iterateTriangles (count - 1)
+          children = getChildren triangle
+
 triangle = UG.getEqTriangleAtOrigin 100
 
 run :: IO ()
 run = G.display (G.InWindow "Fractals" (500, 500) (20, 20)) G.black $ G.Pictures $
-    map (renderTriangle (G.Color G.blue)) (getChildren triangle)
-    ++ [renderTriangle (G.Color G.red) triangle]
+    map (renderTriangle (G.Color G.blue)) (iterateTriangles 6 triangle)
